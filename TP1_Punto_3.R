02da4bb4068b8140281cf341cef6d69a8358f0bb
Tavo <gariosp@unal.edu.co>
2018-05-13 22:01:24 -0300

    Punto 3 base added from punto 4

---
+++ TP1_Punto_3.R
@@ -0,0 +1,125 @@
+
+
+rm(list=ls())
+
+setwd("C:/Users/remba/Documents/Maestría DM/AA/TP1/Final/R/4")
+a_train_original_data <- "train_original.csv"
+a_test_original_data <- "test_original.csv"
+library("RWeka")
+library("partykit")
+#install.packages("libcoin")
+#Importo los archivos
+
+df_train_original <- read.csv(a_train_original_data, 
+                              header = TRUE, 
+                              sep = ",", 
+                              colClasses=c('numeric','factor','factor','factor','factor','factor','factor','factor','factor','factor','factor'), 
+                              check.names = FALSE)
+
+df_test_original <- read.csv(a_test_original_data, 
+                             header = TRUE, 
+                             sep = ",", 
+                             colClasses=c('numeric','factor','factor','factor','factor','factor','factor','factor','factor','factor','factor'), 
+                             check.names = FALSE)
+
+
+#Genera árbol train
+tree_train <- J48(as.factor(y)~., df_train_original,control=Weka_control(M=2,C=0.25))
+summary(tree_train)
+performance_train<-summary(tree_train)$details[1]
+
+#Acá evalúa con la base de TEST
+validacion_test<-evaluate_Weka_classifier(tree_train,newdata=df_test_original)
+performance_test<-validacion_test$details[1]
+
+#Dimensión del árbol
+p <- as.party(tree_train)
+leaves<-length(p)
+## [1] 9
+size<-width(p)
+## [1] 5
+prof<-depth(p)
+## [1] 4
+
+
+
+#Genera el árbol con la base de TRAIN
+
+datos_train<-length(df_train_original$y)
+M_porc<-list(seq(0.005,0.1,0.005))
+M_par<-lapply(datos_train*seq(0.005,0.1,0.005),floor)
+hasta<-length(M_par)
+#Genero los árboles
+
+
+
+
+for (i in 1:hasta){
+  tree_train<-J48(as.factor(y)~., df_train_original,control=Weka_control(M=M_par[i],C=0.25))
+  validacion_test<-evaluate_Weka_classifier(tree_train,newdata=df_test_original)
+  p <- as.party(tree_train)
+  
+    if (i==1) {
+    df_punto_4 <- data.frame(Corrida=i,
+                                    M_porcentaje=M_porc[[1]][i],
+                                   M_num=M_par[[i]],
+                                   Performance_train=summary(tree_train)$details[1],
+                                   Size_tree=width(p),
+                                   Num_leaves=length(p),
+                                   Prof_Tree=depth(p),
+                                   Performance_test=validacion_test$details[1])
+    } else {
+  df_punto_4 <- rbind(df_punto_4,
+                                  data.frame(
+                                  Corrida=i,
+                                  M_porcentaje=M_porc[[1]][i],
+                                  M_num=M_par[[i]],
+                                  Performance_train=summary(tree_train)$details[1],
+                                  Size_tree=width(p),
+                                  Num_leaves=length(p),
+                                  Prof_Tree=depth(p),
+                                  Performance_test=validacion_test$details[1],
+                                  check.names=FALSE))
+    }
+}
+
+
+
+tiff(filename="4_Tamanio_vs_MinNumObj.jpeg",units="in",width=6.5,height=4,res=300,compression='lzw')
+ggplot(df_punto_4, 
+       aes(M_porcentaje, Size_tree)) + 
+  geom_line(color='dark blue') + 
+  geom_point(color='dark blue') + 
+  theme_minimal() +
+  xlab("Porcentaje aplicado para el mínimo número de elementos en las hojas") + 
+  ylab("Tamaño del árbol") + 
+  ggtitle("Tamaño según variación del MínNumObj")+
+  theme_linedraw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+dev.off()
+
+df_graf<-rbind(data.frame(df_punto_4[,c(1:3)],Performance=df_punto_4[,4],Base=rep("Train",20)),
+               data.frame(df_punto_4[,c(1:3)],Performance=df_punto_4[,8],Base=rep("Test",20)))
+
+tiff(filename="4_Accuracy_vs_MinNumObj.jpeg",units="in",width=6.5,height=4,res=300,compression='lzw')
+ggplot(df_graf, 
+       aes(M_porcentaje, Performance, group = Base, color = Base)) + 
+  geom_line() + 
+  geom_point() + 
+  theme_minimal() +
+  xlab("Porcentaje aplicado para el mínimo número de elementos en las hojas") + 
+  ylab("Tamaño del árbol") + 
+  ggtitle("Accuracy según variación del MínNumObj")+
+  theme_linedraw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+dev.off()
+
+write.table(x = df_punto_4,
+            file = "Resultados_punto_4_MinNumObj.csv", 
+            sep = ",", 
+            quote = FALSE,
+            row.names = FALSE)
